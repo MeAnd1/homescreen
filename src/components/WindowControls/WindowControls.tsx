07c9747910218076559bb5e23c749921c0d8a68e
VanLH23 <vanlh23@fpt.com>
2026-04-07 07:22:31 +0700

    Fix window on mobile

--- src/components/WindowControls/WindowControls.tsx
+++ src/components/WindowControls/WindowControls.tsx
@@ -8,15 +8,34 @@ interface WindowControlsProps {
 }
 
 function WindowControls({ onMinimize, onMaximize, onClose }: WindowControlsProps) {
+  const stop = (e: React.PointerEvent | React.MouseEvent | React.TouchEvent) => {
+    e.stopPropagation()
+  }
+
+  const handleClose = (e: React.MouseEvent | React.TouchEvent) => {
+    e.stopPropagation()
+    e.preventDefault()
+    onClose?.()
+  }
+
   return (
-    <div className="window-controls">
+    <div
+      className="window-controls"
+      onPointerDown={stop}
+      onMouseDown={stop}
+      onTouchStart={stop}
+    >
       <button className="window-control-btn" onClick={onMinimize}>
         <Minus size={12} />
       </button>
       <button className="window-control-btn" onClick={onMaximize}>
         <Square size={10} />
       </button>
-      <button className="window-control-btn window-control-close" onClick={onClose}>
+      <button
+        className="window-control-btn window-control-close"
+        onClick={handleClose}
+        onTouchEnd={handleClose}
+      >
         <X size={14} />
       </button>
     </div>
